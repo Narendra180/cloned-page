a84a8e1b186bfe54c4e80dba6553bac1262cc915
Narendra <narendrakanigiri18@gmail.com>
2021-09-28 15:46:35 +0530

    completed mobile nav bar

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 let mobileMenuBtn = document.querySelector(".mobile-menu-btn");    
-let headerNav = document.querySelector(".header-nav");
+let headerNav = document.querySelector(".navbar-item2");
+let navBar = document.querySelector(".navbar");
 
 mobileMenuBtn.addEventListener('click', () => {
-    headerNav.classList.toggle("header-nav-active");
+    headerNav.classList.toggle("display-header-nav");
 });
+
+let prevScrollPosition = window.pageYOffset;
+window.addEventListener("scroll", () => {
+    let currentScrollPosition = window.pageYOffset;
+    if (prevScrollPosition > currentScrollPosition) {
+        navBar.style.top = "0";
+    } else {
+        navBar.style.top = "-55px";
+    }
+    prevScrollPosition = currentScrollPosition;
+})
