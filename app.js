ade66dad5243b0b5662b7ab2cc7f922f224cb039
Narendra <narendrakanigiri18@gmail.com>
2022-01-21 21:07:19 +0530

    added background video for hero section and made changes for contact form

--- app.js
+++ app.js
@@ -116,57 +116,58 @@ scrollToTopBtn.addEventListener('click', () => {
 
 
 
+// background video code
+
+if(window.innerWidth > 767) {
+    let tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 
 
 
+    let section = {
+    start: 0,
+    end: 11
+    };
 
-{/* <div id="myvideo"></div> */}
-
-let player;
-let videoId='hWcLEr75coc';
-let startSeconds = 0;  // set your own video start time when loop play
-let endSeconds = 9;   // set your own video end time when loop play
-let playerConfig = {
-  height: '560',
-  width: '315',
-  videoId: videoId,
-  playerVars: {
-
-    autoplay: 1,            // Auto-play the video on load
-    controls: 0,            // Show pause/play buttons in player
-    showinfo: 0,            // Hide the video title
-    modestbranding: 1,      // Hide the Youtube Logo
-    fs: 1,                  // Hide the full screen button
-    cc_load_policy: 0,      // Hide closed captions
-    iv_load_policy: 3,      // Hide the Video Annotations
-    start: startSeconds,
-    end: endSeconds,
-    autohide: 0, // Hide video controls when playing
-  },
-  events: {
-       'onStateChange': onStateChange,       // reference to Iframe API
-        onReady: function(e) {              // mute the video when loaded
-        e.target.mute();             
-      }
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    let player;
+    function onYouTubeIframeAPIReady() {
+    player = new YT.Player(
+        'player',
+        {
+        height: '100%',
+        width: '100%',
+        videoId: 'hWcLEr75coc',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        },
+        playerVars: {
+            'controls': 0,
+            'mute': 1
+        }
+        }
+    );
     }
-};
-//excute the video in div
-function onYouTubePlayerAPIReady() {
 
-  player = new YT.Player('background-video', playerConfig);
+    function onPlayerReady(event) {
+    player.seekTo(section.start);
+    player.playVideo();
+    }
 
+    function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+        var duration = section.end - section.start;
+        setTimeout(restartVideoSection, duration * 1000);
+    }
+    }
+
+    function restartVideoSection() {
+    player.seekTo(section.start);
+    }
 }
-//repload the video when onStateChange=YT.PlayerState.ENDED)
-function onStateChange(state) {
-  if (state.data === YT.PlayerState.ENDED) {
-    player.loadVideoById({
-      videoId: videoId,
-      startSeconds: startSeconds,
-      endSeconds: endSeconds
-
-    });
-  }
-}
-
-
